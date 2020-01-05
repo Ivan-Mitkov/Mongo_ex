73f27188f7100cee7eedf7a5b8221f122a8c40a1
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-01-05 15:35:48 +0200

    delete records

---
+++ s/users/test/delete_test.js
@@ -0,0 +1,72 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Deleting a user", () => {
+  let joe;
+  //first insert record after it's done do the other tests
+  beforeEach(done => {
+    //_id is assigned here before saving to the DB
+    // Mongoose documents represent a one-to-one mapping to documents as stored in MongoDB. Each document is an instance of its Model.
+    joe = new User({ name: "Joe" });
+    joe.save().then(() => done());
+  });
+
+  it("model instance remove", done => {
+    joe
+      .remove()
+      .then(() => {
+        return User.findOne({ name: "Joe" });
+      })
+      .then(user => {
+        assert(user === null);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
+  });
+
+  it("class method deleteOne", done => {
+    User.deleteOne({ name: "Joe" })
+      .then(() => {
+        return User.findOne({ name: "Joe" });
+      })
+      .then(user => {
+        assert(user === null);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
+  });
+  //   deprecated findOneAndRemove without { useFindAndModify: false }
+  //   findOneAndRemove() becomes a MongoDB findAndModify() command,
+  //as opposed to a findOneAndDelete() command
+  it("class method findOneAndDelete", done => {
+    User.findOneAndDelete({ name: "Joe" })
+      .then(() => {
+        return User.findOne({ name: "Joe" });
+      })
+      .then(user => {
+        assert(user === null);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
+  });
+
+  it("class method deleteMany", done => {
+    User.deleteMany({ name: "Joe" })
+      .then(() => {
+        return User.findOne({ name: "Joe" });
+      })
+      .then(user => {
+        assert(user === null);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    done();
+  });
+});
