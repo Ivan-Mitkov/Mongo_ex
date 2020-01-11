a87aac914de66c01144d723ed1469603a6aabf9d
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-01-11 14:46:18 +0200

    create shemas and a model

--- s/UpStarMusic/database/models/album.js
+++ s/UpStarMusic/database/models/album.js
@@ -1,1 +1,17 @@
 // Todo: create Album Schema
+// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema=mongoose.Schema;
+
+const AlbumSchema=new Schema({
+    title:String,
+    date:Date,
+    copiesSold:Number,
+    numberTracks:Number,
+    image:String,
+    revenue:Number,
+  
+})
+
+//it's subdocument so there is NO MODEL
+module.exports=AlbumSchema;
