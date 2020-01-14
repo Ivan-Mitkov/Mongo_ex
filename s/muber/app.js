d8da695d1f83afd2eda1ef00c36b4aaf52914a90
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-01-14 16:21:33 +0200

    error handling and middleware

--- s/muber/app.js
+++ s/muber/app.js
@@ -1,15 +1,35 @@
-const express =require("express")
-const mongoose=require('mongoose');
-const routes=require('./routes/routes');
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
+const app = express();
 
-if(process.env.NODE_ENV!=='test'){
-    mongoose.connect('mongodb://localhost/muber', {useNewUrlParser: true,useUnifiedTopology: true})
-
+if (process.env.NODE_ENV !== "test") {
+    try {
+        mongoose.connect("mongodb://localhost/muber", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          });  
+    } catch (error) {
+        console.warn('Connection', error)
+    }
+  
 }
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-routes(app)
-module.exports=app;
+routes(app);
+//executing middleware for errors after routes
+// Error-handling middleware always takes four arguments.
+// You must provide four arguments to identify it as an error-handling middleware function.
+// Even if you don’t need to use the next object, you must specify
+// it to maintain the signature. Otherwise, the next object will be interpreted
+// as regular middleware and will fail to handle errors.
+app.use((err, req, res, next) => {
+  //err only if previous middleware threw an error 
+  // email: Path `email` is required.
+//   console.log('Error Middleware ',err);
+  res.status(422).send({error:err.message})
+  
+});
+module.exports = app;
--- s/muber/controllers/drivers_controller.js
+++ s/muber/controllers/drivers_controller.js
@@ -5,11 +5,18 @@ module.exports = {
   greeting: (req, res) => {
     res.send({ hi: "there" });
   },
-  create: async (req, res) => {
-    console.log(req.body);
+  create: async (req, res,next) => {
+    console.log('req body: ',req.body);
     //props to create new driver
     const driversProps = req.body;
-    const newDriver = await Driver.create(driversProps);
-    res.send(newDriver);
+    try {
+        const newDriver = await Driver.create(driversProps);
+        res.send(newDriver);
+    } catch (error) {
+        //if there is error for example missing required fields
+        // call next middleware error handling middleware in app.js
+        next(error)
+    }
+   
   }
 };
